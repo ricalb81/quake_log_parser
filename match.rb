9aa256c516a1e1d0120d8ba1be588e8780fbd54b
Ricardo Albuquerque <ricalb@gmail.com>
2024-04-23 08:06:57 -0300

    Create class Match

--- match.rb
+++ match.rb
@@ -10,4 +10,15 @@ class Match
     @kills_by_means = {}
     @total_kills = 0
   end
+
+  def report
+    {
+      "game_#{@id}": {
+        "total_kills": @total_kills,
+        "players": @players,
+        "kills": @kills,
+        "kills_by_means": @kills_by_means
+      }
+    }
+  end
 end
